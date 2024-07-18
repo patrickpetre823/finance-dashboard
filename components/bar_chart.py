539603d30e3117cb85072c5f4bf47112719a8a80
Patrick Petre <patrickpetre823@yahoo.de>
2024-07-18 21:38:03 +0200

    added Bar Chart -> No Data -> Function not called?

--- components/bar_chart.py
+++ components/bar_chart.py
@@ -7,6 +7,33 @@ from . import ids
 
 def render(app: Dash, data: pd.DataFrame) -> html.Div:
     
-    fig = px.bar(data, x=data["monat"], y=data['Betrag (€)'], color='consumption_categories')
+    @app.callback(
+        Output(ids.BAR_CHART, "children"),
+        Input(ids.YEAR_DROPDOWN, "value")
+    )
+    def update_bar_chart(years: list[str]) -> html.Div:
+        filtered_data = data.query("year in @years")
+        if filtered_data.shape[0] == 0:
+            return html.Div("No Data selected")
+        
+        def create_privot_table() -> pd.DataFrame:
+            pt = filtered_data.pivot_table(
+                  values=data["Betrag (€)"],
+                  index=data['consumption_categories'],
+                  aggfunc="sum",
+                  fill_value=0
+            )
+            return pt.reset_index().sort_values("Betrag (€)", ascending=False)
+        
+        fig = px.bar(
+              create_pivot_table(),
+              x=data['consumptions_categories'],
+              y=data['Betrag (€)'],
+              color=data["consumption_categories"]
+        )
+        return html.Div(dcc.Graph(figure=fig), id=ids.BAR_CHART)
+    # 
+    # 
+    # fig = px.bar(data, x=data["monat"], y=data['Betrag (€)'], color='consumption_categories')
 
-    return html.Div(dcc.Graph(figure=fig), id= ids.BAR_CHART)
+    return html.Div(id= ids.BAR_CHART)
