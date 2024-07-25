1d07d54fc9061b2d614308f286c5ab80205c34e4
Patrick Petre <patrickpetre823@yahoo.de>
2024-07-25 15:32:59 +0200

    added pie chart

--- components/pie_chart.py
+++ components/pie_chart.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.express as px
 from . import ids
 import plotly.graph_objects as go
+from math import *
 
 def render(app: Dash, data: pd.DataFrame) -> html.Div:
 
@@ -22,24 +23,25 @@ def render(app: Dash, data: pd.DataFrame) -> html.Div:
     #print(df_mean)
 
     labels = list(df_mean.keys())
+    
     values = list(df_mean.values())
-
     values = list(map(float, values))
+    values = [fabs(value) for value in values ]
 
-    print("---------------------------------------")
-    print(values)
-    print(labels)
+    sum_avg = round(sum(values), 0)
+    sum_avg = '{:,}'.format(sum_avg)
+
+    #fig = px.pie(values=values, names=labels)
     fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
     
-    layout = go.Layout(
-                   title="Percentage of events",
-                   font=dict(family='Arial', size=12, color='#909090'),
-                   legend=dict(x=0.9, y=0.5)
+
+    fig.update_layout(title=f"Total Money per Month: {sum_avg} €",
+                  font=dict(family='Arial', size=12, color='#909090'),
+                   legend=dict(x=2, y=0.5)
                     )
 
-    fig.show()
     
-    pie_chart = html.Div(dcc.Graph(figure=fig), id=ids.PIE_CHART)
+    pie_chart = html.Div(dcc.Graph(figure=fig, style={'width': '90vh', 'height': '90vh'}), id=ids.PIE_CHART,)
 
 
     return html.Div(children=[html.H6('Average Money Spent Per Month'), pie_chart],)
