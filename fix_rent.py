1b7407f1d0e7d0da3acc23f990a55c75a99859e8
Patrick Petre <patrickpetre823@yahoo.de>
2024-08-14 18:55:25 +0200

    fix month in progress

--- fix_rent.py
+++ fix_rent.py
@@ -8,16 +8,24 @@ def fix_rent(data: pd.DataFrame ) -> pd.DataFrame:
     So find month that has no rent, then take rent of next month and put it a month earlier
     """
     unique_months = data['monat'].unique()
+    df = data
     print(unique_months)
     for month in unique_months:
-        print(month)
+        
         #df = data[month]
-        df = df[df['Zahlungsempfänger*in'] == 'Hans Schneider']
+        print('-------------------FIX RENT ---------------------------')
+
+        df_1 = df[(df['Zahlungsempfänger*in'] == 'Hans Schneider') & (df['monat'] == 'January')]
+        df = df[(df['Zahlungsempfänger*in'] == 'Hans Schneider') & (df['monat'] == month)]
+        #df = df[df['monat'] == 'January']
+        print(month + type(month))
+        print(type(month))
+        print(type('january'))
         print(df)
-        if df != None:
-            break
-        else:
-            break
+        #if df != None:
+        #    break
+        #else:
+        #    break
 
         
 
